2dc1fd171117d0a66183094e868fef1561925bab
CryWolf <cry.wolf@centrum.cz>
2016-03-30 00:52:09 +0200

    Initial commit

---
+++ imports/ui/body.js
@@ -0,0 +1,13 @@
+import { Template } from 'meteor/templating';
+
+import './body.html';
+
+Template.body.helpers({
+  tasks: [
+    { text: "Todo task 1" },
+    { text: "Todo task 2" },
+    { text: "Todo task 3" },
+    { text: "Todo task 4" },
+    { text: "Todo task 4" }
+  ]
+});
