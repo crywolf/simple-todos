af2bc128071fa5d264fc2c591dbe0c9ac67cd2fd
CryWolf <cry.wolf@centrum.cz>
2016-04-05 08:24:27 +0200

    Tasks in Mongodb

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,49 @@
 import { Template } from 'meteor/templating';
+import { Tasks } from '../api/tasks';
+import moment from 'moment';
+// import 'moment/locale/cs'; // workaround for bug that prevents moment.locale from being loaded
 
 import './body.html';
+import './task.html';
 
 Template.body.helpers({
-  tasks: [
-    { text: 'Todo task 1' },
-    { text: 'Todo task 2' },
-    { text: 'Todo task 3' },
-    { text: 'Todo task 4' },
-    { text: 'Todo task 4' }
-  ]
+  tasks () {
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  }
+});
+
+Template.body.events({
+  'submit #new-task' (event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    Tasks.insert({
+      text,
+      createdAt: new Date()
+    });
+
+    target.text.value = '';
+  }
+});
+
+Template.task.helpers({
+  createdAtFormatted () {
+    return moment(this.createdAt).fromNow();
+  }
+});
+
+Template.task.events({
+  'click .toggle-checked' () {
+    Tasks.update(this._id, {
+      $set: {
+        checked: ! this.checked
+      }
+    });
+  },
+
+  'click button.delete' () {
+    Tasks.remove(this._id);
+  }
 });
